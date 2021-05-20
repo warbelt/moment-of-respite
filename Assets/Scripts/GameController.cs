cd99d31b4f73a41ff2a785d2040100cde76348bc
Mario Durántez <mdurantezs@gmail.com>
2021-05-20 18:38:53 +0200

    [Dev] Add max score system

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class GameController : MonoBehaviour
 {
+    string CONST_PLAYERPREFS_KEY_MAXSCORE = "savedMaxScore";
+
+
     [SerializeField] private PlayerController _player;
     [SerializeField] private UIController _uiController;
     [SerializeField] private ParticleSystem _bgParticles;
@@ -14,6 +16,17 @@ public class GameController : MonoBehaviour
 
     //State
     bool _isPlaying;
+    int _maxScore;
+    int MaxScore
+    {
+        get => _maxScore;
+        set
+        {
+            _maxScore = value;
+            SetMaxScore(_maxScore);
+        }
+    }
+
     int _gameScore;
     int GameScore
     {
@@ -30,6 +43,7 @@ public class GameController : MonoBehaviour
     {
         _isPlaying = false;
         _player.enabled = false;
+        MaxScore = LoadMaxScore();
     }
 
     private void Start()
@@ -44,6 +58,11 @@ public class GameController : MonoBehaviour
 
         // Start game
         StartCoroutine(StartRound());
+    }
+
+    private void OnDestroy()
+    {
+        SaveMaxScore();
     }
 
     public void StartGame()
@@ -108,9 +127,23 @@ public class GameController : MonoBehaviour
         _uiController.SetScoreText(score);
     }
 
+    private void SetMaxScore(int maxScore)
+    {
+        _uiController.SetMaxScoreText(maxScore);
+    }
+
+    private void CheckMaxScore()
+    {
+        if (GameScore > MaxScore)
+        {
+            MaxScore = GameScore;
+        }
+    }
+
     private void PlayerDead()
     {
-        _uiController.ActivateReplayButton();
+        _uiController.ActivateReplayMenu();
+        CheckMaxScore();
     }
 
     private void ReplaySignal()
@@ -140,5 +173,15 @@ public class GameController : MonoBehaviour
     private void PointsGainedHandler(int points)
     {
         GameScore += points;
+    }
+
+    private void SaveMaxScore()
+    {
+        PlayerPrefs.SetInt(CONST_PLAYERPREFS_KEY_MAXSCORE, MaxScore);
+    }
+
+    private int LoadMaxScore()
+    {
+        return PlayerPrefs.GetInt(CONST_PLAYERPREFS_KEY_MAXSCORE, 0);
     }
 }
