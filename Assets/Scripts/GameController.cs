f0fef48c6265aa619b77b0a155ba04afa0e4fcb3
Mario Durántez <mdurantezs@gmail.com>
2021-08-20 20:50:35 +0200

    [Fix] Fix reset issue

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -179,6 +179,7 @@ public class GameController : MonoBehaviour
 
         StopAllCoroutines();
         _musicManager.ActivateGamePlayMusic();
+        _activeCoroutine = StartCoroutine(StartRound());
 
     }
 
